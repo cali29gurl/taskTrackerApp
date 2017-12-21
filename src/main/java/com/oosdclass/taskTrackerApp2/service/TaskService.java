a81423284f127913ad78643d20e14e0064983c1a
schumacher <schumacher@DESKTOP-GTFSLKV.fec.org>
2017-12-21 14:31:05 -0600

    task service

---
+++ src/main/java/com/oosdclass/taskTrackerApp2/service/TaskService.java
@@ -0,0 +1,11 @@
+package com.oosdclass.taskTrackerApp2.service;
+
+import java.util.List;
+
+import com.oosdclass.taskTrackerApp2.model.Task;
+
+public interface TaskService {
+	
+	List<Task>retrieveAllTasks();
+	
+}
